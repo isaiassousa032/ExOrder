b1589bd973ed670e0cafd10f0588ced47ebacb0e
Isaias Santos <isaiassousa032@gmail.com>
2023-07-26 14:10:31 -0300

    Adicionar arquivos de projeto.

---
+++ Entities/Order.cs
@@ -0,0 +1,50 @@
+﻿using ExProposto.Entities.Enums;
+
+namespace ExProposto.Entities
+{
+    class Order
+    {
+        public DateTime Moment { get; set; }
+        public OrderStatus Status { get; set; }
+        public Client Client { get; set; }
+        public List<OrderItem> Items { get; set; } = new List<OrderItem>();
+
+        public Order()
+        {
+
+        }
+
+        public Order(DateTime moment, OrderStatus status, Client client)
+        {
+            Moment = moment;
+            Status = status;
+            Client = client;
+        }
+
+        public void AddItem(OrderItem item)
+        {
+            Items.Add(item);
+        }
+
+        public void RemoveItem(OrderItem item)
+        {
+            Items.Remove(item);
+        }
+
+
+        public double Total()
+        {
+            //int totalItems = 0;
+            //foreach (OrderItem item in Items)
+            //{
+            //    totalItems += item.Quantity;
+            //}
+            //return totalItems;
+
+            foreach (Order item in Items)
+            {
+                
+            }
+        }
+    }
+}
---
+++ Entities/OrderItem.cs
@@ -0,0 +1,26 @@
+﻿
+
+namespace ExProposto.Entities 
+{
+    class OrderItem
+    {
+        public int Quantity { get; set; }
+        public double Price { get; set; }
+
+        public OrderItem()
+        {
+            
+        }
+
+        public OrderItem(int quantity, double price)
+        {
+            Quantity = quantity;
+            Price = price;
+        }
+
+        public double SubTotal()
+        {
+            return Quantity * Price;
+        }
+    }
+}
